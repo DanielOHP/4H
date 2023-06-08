4e5e5fdd08b02a02592ea190c732a6a675963300
DanielOHP <129417453+DanielOHP@users.noreply.github.com>
2023-06-08 08:34:34 -0600

    eje2

--- DanielOHP/src/app/app-routing.module.ts
+++ DanielOHP/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Eje1Component } from './ejercicios/eje1/eje1.component';
+import { Eje2Component } from './ejercicio/eje2/eje2.component';
 
 
 const routes: Routes = [
 {path:'eje1',component:Eje1Component},
-{path:""}
+{path:'Home',component:Eje2Component}
+
+
 
 
 
